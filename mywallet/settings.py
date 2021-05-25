b8ccc23b9b8b7070307e9b242966a4610feff3e1
Enos Andrade <enosads@gmail.com>
2021-05-25 19:33:41 -0300

    ALLOWED_HOSTS updated

--- mywallet/settings.py
+++ mywallet/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-hegaeu_hukovfi$yb(gq)!k^ouiz^jw15n63@jgzhafvu*jp8@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
